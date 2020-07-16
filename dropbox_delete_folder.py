cccb07f25ca25c19b26c5113e2986a6dbfce24eb
Laurens van der Kooi <lvvdkooi@hotmail.com>
2020-07-16 18:23:09 +0200

    Initial commit

---
+++ dropbox_delete_folder.py
@@ -0,0 +1,18 @@
+import dropbox
+from datetime import date, timedelta
+
+DAGEN_GRENS = 7
+TOKEN = "jYeEYD8vu1oAAAAAAAAW9dP53LLFNiqd5Nd43yNi5rvLDnrtWY3umiraomLSj_-m"
+
+print("Initializing Dropbox API...")
+dbx = dropbox.Dropbox(TOKEN)
+
+vorige_week_string = str(date.today() - timedelta(days=DAGEN_GRENS))
+
+print("Verwijderen van de map {}".format(vorige_week_string))
+
+try:
+    dbx.files_delete_v2(path="/PiCam/{}".format(vorige_week_string))
+    print("Verwijderen van map {} succesvol.".format(vorige_week_string))
+except Exception:
+    print("Er iets mis gegaan, de map is waarschijnlijk reeds verwijderd.")
